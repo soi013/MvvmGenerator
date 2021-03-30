a2ed4d4304cd9f73c25be84e59414be440171a57
soi013 <soi2flatline@gmail.com>
2021-03-30 23:09:18 +0900

    Add WpfTest project

---
+++ MvvmGenerator.WpfTests/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmGenerator.WpfTests
+{
+    public partial class MainWindowViewModel
+    {
+        [AutoNotify("Xxxx")]
+        private string _firstName;
+
+        [AutoNotify]
+        private string _lastName;
+
+        [AutoNotify]
+        public string FullName => $"{Xxxx} {LastName}";
+    }
+}
